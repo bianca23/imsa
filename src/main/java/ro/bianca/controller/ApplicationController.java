8b9fd3c314ed4aaebc75d1c0ca5c1a88e63e45c2
v-almusc <v-almusc@microsoft.com>
2016-09-01 14:39:34 +0300

    added data to view

--- src/main/java/ro/bianca/controller/ApplicationController.java
+++ src/main/java/ro/bianca/controller/ApplicationController.java
@@ -2,6 +2,7 @@ package ro.bianca.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,13 +10,14 @@ public class ApplicationController {
 
     @RequestMapping("/erp/it")
     @PreAuthorize("hasRole('ROLE_IT')")
-    public String itView() {
+    public String itView(Model model) {
+        model.addAttribute("example", "this is an example");
 
         return "it";
     }
 
     @RequestMapping("/erp/hr")
-    @PreAuthorize("hasRole('ROLE_IT')")
+    @PreAuthorize("hasRole('ROLE_HR')")
     public String hrView() {
 
         return "hr";
